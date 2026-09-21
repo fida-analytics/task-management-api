39320d1798aebf6f3cdddd98f7c0b596919aafa7
fida-analytics <fidam3647@gmail.com>
2026-09-21 23:12:47 +0500

    Fix deployment port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 
 // Rollout.host ke liye PORT
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(express.json());
